e8cbe8e2a393923b36b079f6fe55fd1448556e06
Mas-Pey <konamexxxpes@gmail.com>
2025-11-21 15:38:49 +0700

    feat: add main entry point with gin cors

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,28 @@
 package main
 
+import (
+	"go-wedding/features"
+	"go-wedding/utils"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	var today int = 230 + 23
-	println(today)
+	db := utils.ConnectDB()
+	defer db.Close()
+	db.SetConnMaxIdleTime(0)
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(10)
+	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
+	r = features.AuthRoute(r, db)
+	r.Run()
 }
