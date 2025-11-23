f14a4dd9078119504954ccb9786f4bf7666825c4
Mas-Pey <konamexxxpes@gmail.com>
2025-11-23 13:43:47 +0700

    feat: apply auth middleware

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"go-wedding/features"
+	"go-wedding/middleware"
 	"go-wedding/utils"
+	"net/http"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -14,6 +16,7 @@ func main() {
 	db.SetConnMaxIdleTime(0)
 	db.SetMaxIdleConns(10)
 	db.SetMaxOpenConns(10)
+
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
 		AllowAllOrigins:  true,
@@ -22,7 +25,17 @@ func main() {
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 	}))
-
 	r = features.AuthRoute(r, db)
+
+	protected := r.Group("/api")
+	protected.Use(middleware.RequireAuth)
+	protected.GET("/me", func(ctx *gin.Context) {
+		userID, _ := ctx.Get("userID")
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Access dashboard successful",
+			"user_id": userID,
+		})
+	})
+
 	r.Run()
 }
